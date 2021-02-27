8de624a2e4023dd2f5ccdad51e3d352ce41ccc84
Bluedoy <support@bluedoy.com>
2021-02-27 18:37:36 +0000

    updates

--- counter-redux/src/App.js
+++ counter-redux/src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import Counter from './components/Counter'
-import Vote from './components/Vote'
+import "./App.css";
+import Counter from "./components/Counter";
+import Vote from "./components/Vote";
 
 function App() {
+  const votes = ["John", "Bill", "Alex", "James"];
   return (
     <div className="App">
-      <Counter />
       <Vote />
+      <div style={{margin: "30px"}}>
+        {votes.map((vote) => {
+          return <Counter name={vote} />;
+        })}
+      </div>
     </div>
   );
 }
--- counter-redux/src/components/Counter.jsx
+++ counter-redux/src/components/Counter.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { decrement, increment } from "../redux/ducks/counter";
 
+const Counter = (props) => {
+  const { name } = props;
 
-const Counter = () => {
+  const [count, setCount] = useState(0);
   /* const count = useSelector((state) => state.counter.count); */
   const dispatch = useDispatch();
 
   const handleIncrement = () => {
     dispatch(increment());
+    setCount(count + 1);
   };
 
   const handleDecrement = () => {
-    dispatch(decrement())
+    dispatch(decrement());
+    setCount(count - 1);
   };
 
   return (
-    <div>
-      <h1>Counter</h1>
-      {/* <h1>{count}</h1> */}
+    <div style={{backgroundColor: "grey"}}>
+      <h1>{name}</h1>
+      <h1>{count}</h1>
       <button onClick={handleIncrement}>+</button>
       <button onClick={handleDecrement}>-</button>
     </div>
